f32a6540d9c61fc908c8857d0a2dc518f986e706
thewayofyomi <sean.humphreys808@outlook.com>
2023-10-07 19:20:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pythonSeries
---
+++ lesson03/meaning.py
@@ -0,0 +1,10 @@
+meaning = 42
+print("")
+
+# if meaning > 10:
+#     print("Right On!")
+# else:
+#     print("Not Today")
+
+# Ternary Operator
+print("Right On!") if meaning > 10 else print("Not Today")
